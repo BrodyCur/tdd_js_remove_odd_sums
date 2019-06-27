28a899e377ff4732bfe62af2bdf2d0f1c8dc5828
Brody Currie <brodycurrie@gmail.com>
2019-06-27 12:47:16 -0400

    completed reinforcement exercise

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 function removeOddSum() {
-}
+  const compareObject = [ {a: 5, b: 5},
+    {a: 3, b: 4},
+    {a: 2, b: 0},
+    {a: 2, b: 1}
+  ]
+
+  let evenArray = [];
+
+    compareObject.forEach((obj) => {
+      if ((obj.a - obj.b) % 2 === 0) {
+        evenArray.push(obj);
+      };
+    });
+    return evenArray
+};
 
 module.exports = removeOddSum;
